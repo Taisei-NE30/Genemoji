d8a9cce24a307b3d1529429980ad54e88d791cbd
Taisei Nakajima <ne300085@senshu-u.jp>
2021-02-01 03:25:30 +0900

    Load font from s3

--- src/functions/generateEmoji/handler.ts
+++ src/functions/generateEmoji/handler.ts
@@ -1,9 +1,26 @@
-import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
+import { formatJSONResponse, ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
+import AWS from 'aws-sdk'
 import { middyfy } from '@libs/lambda';
+import { registerFont } from 'canvas';
 import schema from './schema';
 
 const generateEmoji: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  const s3 = new AWS.S3()
+  const response = s3.getObject({
+    Bucket: 'genemoji-fonts',
+    Key: 'NotoSansMonoCJKJP_Bold.ttf'
+  }, (err, data) => {
+    if (err) {
+      console.error(err)
+    }
+    registerFont(data.Body.toString(),  {
+      family: 'Noto Sans JP'
+    })
+  })
 
+  return formatJSONResponse({
+
+  });
 }
 
 export const main = middyfy(generateEmoji)
