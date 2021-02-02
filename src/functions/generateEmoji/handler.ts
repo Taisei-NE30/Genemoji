dd6e74081846218ac8dbffd6ad607a2bcbb64f1e
Taisei Nakajima <ne300085@senshu-u.jp>
2021-02-02 22:58:23 +0900

    Change api

--- src/functions/generateEmoji/handler.ts
+++ src/functions/generateEmoji/handler.ts
@@ -6,12 +6,13 @@ import schema from './schema';
 
 const generateEmoji: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   const s3 = new AWS.S3()
-  const response = s3.getObject({
+  s3.getObject({
     Bucket: 'genemoji-fonts',
     Key: 'NotoSansMonoCJKJP_Bold.ttf'
   }, (err, data) => {
     if (err) {
       console.error(err)
+      return
     }
     registerFont(data.Body.toString(),  {
       family: 'Noto Sans JP'
@@ -19,7 +20,7 @@ const generateEmoji: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
   })
 
   return formatJSONResponse({
-
+    message: "OK"
   });
 }
 
--- src/functions/generateEmoji/index.ts
+++ src/functions/generateEmoji/index.ts
@@ -5,8 +5,8 @@ export default {
   events: [
     {
       http: {
-        method: 'post',
-        path: 'hello',
+        method: 'get',
+        path: 'generate',
         request: {
           schema: {
             'application/json': schema
